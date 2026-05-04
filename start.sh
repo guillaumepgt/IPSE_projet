4ed78e3f92b304ddfa19698d6eee78305e8d624e
guillaumepgt <guillaume.prigent44@gmail.com>
2026-05-04 17:29:56 +0200

    reforme: start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "=== Nettoyage ==="
 pkill -9 -f "go"
 rm -f go.txt
 
@@ -10,7 +9,6 @@ ROBOTSManuel=("MRPiZ white")
 
 PORT=12301
 
-echo "=== COMPILATION ==="
 make clean
 make
 
@@ -19,16 +17,12 @@ if [ ! -f "go" ]; then
     exit 1
 fi
 
-echo "=== LANCEMENT DES IA (Webots gère déjà le réseau) ==="
-
-# 1. On connecte les 6 robots AUTO
 for robot in "${ROBOTSAuto[@]}"; do
     echo "Connexion de $robot au port $PORT (AUTO)"
     ./go $PORT auto &
     PORT=$((PORT + 1))
 done
 
-# 2. On connecte les 5 robots FILE
 for robot in "${ROBOTSFile[@]}"; do
     echo "Connexion de $robot au port $PORT (FILE)"
     ./go $PORT file &
@@ -37,11 +31,8 @@ done
 
 sleep 0.5
 touch go.txt
-echo "GOOOOO !!!"
 
-# 3. On connecte le robot MANUEL (au premier plan pour garder le clavier)
 for robot in "${ROBOTSManuel[@]}"; do
-    echo ">>> VOUS AVEZ LES COMMANDES SUR '$robot' (Port $PORT) <<<"
     ./go $PORT manual
     PORT=$((PORT + 1))
 done
